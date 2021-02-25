1333c0de2c0d78e4bb8faaf146a1224140fd6fbe
胡子豪 <wuqieor@outlook.com>
2021-02-25 16:42:28 +0800

    斐波那契数列带缓存

---
+++ main/fibwithcache.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+const Len = 41
+
+var cache [Len]int
+
+func CachedFib(n int) int {
+	if n <= 1 {
+		return n
+	}
+	if cache[n] == 0 {
+		cache[n] = CachedFib(n-1) + CachedFib(n-2)
+	}
+	return cache[n]
+}
+
+func init() {
+	DefaultLogger.Log()
+	for i := 0; i <= Len; i++ {
+		fmt.Printf("%v: %v\n", i, CachedFib(i))
+	}
+}
