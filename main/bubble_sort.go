93faa1967469688a67b62a3fc62e351deb48d3c2
胡子豪 <wuqieor@outlook.com>
2021-02-25 23:59:42 +0800

    最优的单向冒泡排序[添加测试&typos]

--- main/bubble_sort.go
+++ main/bubble_sort.go
@@ -1,10 +1,12 @@
 package main
 
+import "fmt"
+
 // 最优的单向冒泡排序
 func bubbleSort(a []int) {
 	for n := len(a); n > 1; {
 		newn := 0 // 用来保证一定退出最外层循环
-		for i := 0; i < n; i++ {
+		for i := 1; i < n; i++ {
 			if a[i-1] > a[i] {
 				a[i-1], a[i] = a[i], a[i-1]
 				newn = i // 用来跳过比较已排序的元素
@@ -12,4 +14,11 @@ func bubbleSort(a []int) {
 		}
 		n = newn
 	}
+}
+
+func init() {
+	DefaultLogger.Log()
+	a := []int{4, 2, 3, 5, 1}
+	bubbleSort(a)
+	fmt.Println(a)
 }
